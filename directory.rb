65f77c59109ef9ed643558ccab2435d22a818b01
Luke Wickens <lukewickens1989@gmail.com>
2020-11-15 10:20:30 +0000

    Completion of Step 14 exercises

--- directory.rb
+++ directory.rb
@@ -1,56 +1,46 @@
-@students = [] # an empty array accessible to all methods
+require "csv"
+@students = [] 
 
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
   puts "4. Load the list from students.csv"
-  puts "9. Exit" # 9 because we'll be adding more items
+  puts "9. Exit" 
 end
 
 def interactive_menu
   loop do
     print_menu
-    process(STDIN.gets.chomp)
+    user_input = menu_selection(STDIN.gets.chomp)
+    puts user_input if user_input != nil
   end
 end
 
-def process(selection)
+def menu_selection(selection)
   case selection
-  when "1"
-    input_students
-  when "2"
-    show_students
-  when "9"
-    exit # this will cause the program to terminate
-  when "3"
-    save_students
-  when "4"
-    load_students
-  else
-    puts "I don't know what you meant, try again"
+    when "1" ; input_students
+    when "2" ; show_students
+    when "3" ; filename = ask_for_file ; save_students(filename)
+    when "4" ; filename = ask_for_file ; load_students(filename)
+    when "9" ; exit
+  else ; return "#{selection} is not recognised. Please enter a selection:"
   end
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # get the first name
   name = STDIN.gets.chomp
-  # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
-    @students << {name: name, cohort: :november}
+    add_student name
     puts "Now we have #{@students.count} students"
-    # get another name from the user
     name = STDIN.gets.chomp
   end
 end
 
 def show_students
-  print_header
-  print_student_list
-  print_footer
+  print_header; print_student_list; print_footer
 end
 
 def print_header
@@ -59,8 +49,10 @@ def print_header
 end
 
 def print_student_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  if @students.length != 0
+    @students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
+  else
+    puts "There are no students!"
   end
 end
 
@@ -68,36 +60,45 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
-def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
-  # iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+def ask_for_file
+  puts "Please enter the file you wish to load:"
+  filename = gets.chomp
+end
+
+def save_students(filename = "students.csv")
+  CSV.open(filename, "w") do |line|
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      line << [student[:name], student[:cohort]]
+    end
   end
-  file.close
+end
+
+def add_student(name, cohort="november")
+  @students << {name: name, cohort: cohort.to_sym}
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-    @students << {name: name, cohort: cohort.to_sym}
+  filename = "students.csv" if filename == ""
+  if File.exists?(filename)
+    CSV.foreach(filename) do |name, cohort|
+      add_student name, cohort
+      next
+    end
+  else
+    puts "The specified file does not exist!"
   end
-  file.close
 end
 
 def try_load_students
-  filename = ARGV.first# first argument from the command line
-  return if filename.nil? # get out of the method if it isn't given
-  if File.exists?(filename) # if it exists
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
     load_students(filename)
-     puts "Loaded #{@students.count} from #{filename}"
-  else # if it doesn't exist
-    puts "Sorry, #{filename} doesn't exist."
-    exit # quit the program
+    puts "Loaded #{@students.count} enteries from #{filename}."
+  else
+    puts "Sorry, #{filename} doesn't exist!"
+    exit
   end
 end
 
